a6606f36a1d073636e28e97f34f6a27b192bd234
Simon Göbel <simon.goebel@pressrelations.de>
2018-05-07 10:22:08 +0200

    fix strange 0-charts

--- src/dataProcessing/dataToPoints.js
+++ src/dataProcessing/dataToPoints.js
@@ -12,8 +12,17 @@ export default ({ data, limit, width = 1, height = 1, margin = 0, max = arrayMax
     const vfactor = (height - margin * 2) / ((max - min) || 2);
     const hfactor = (width - margin * 2) / ((limit || len) - (len > 1 ? 1 : 0));
 
-    return data.map((d, i) => ({
-        x: i * hfactor + margin,
-        y: (max === min ? 1 : (max - d) * vfactor + margin)
-    }));
+  return data.map((d, i) => {
+    const y
+    if ((max == min) && (max === 0)) {
+      y = 1
+    } else {
+      y = (max === min ? 1 : (max - d)) * vfactor + margin
+    }
+
+    return {
+      x: i * hfactor + margin,
+      y: y
+    }
+  });
 };
